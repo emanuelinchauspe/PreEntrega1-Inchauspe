df5777588ce031e261ba40c1760b74f00a53112b
Emanuel Inchauspe <ema.inchauspe@outlook.es>
2023-06-08 00:04:19 -0300

    Se generó el bucle y se probó el funcionamiento

--- javascript.js
+++ javascript.js
@@ -1,7 +1,26 @@
-alert("Vamos a aprender las tablas")
+// let respuesta;
 
-let ingresarNumero = parseInt(prompt("¿Las tablas de qué numéro te gustaría aprender?"));
-for (let i = 1; i <= 10; i++) {
-    let resultado = ingresarNumero * i ;
-    alert(ingresarNumero +" X "+ i +" = "+ resultado);
-}
+// do {
+
+// alert("Vamos a aprender las tablas!")
+
+// let ingresarNumero = parseInt(prompt("¿De qué numéro te gustaría aprender?"));
+// for (let i = 1; i <= 10; i++) {
+//     let resultado = ingresarNumero * i ;
+//     alert(ingresarNumero +" X "+ i +" = "+ resultado);
+// }
+// var respuesta = prompt("¿Deseas volver a comenzar? (responde 'sí' o 'no')");
+// } while (respuesta.toLowerCase() === "sí");
+
+let respuesta;
+do {
+  alert("Vamos a aprender las tablas!");
+
+  let ingresarNumero = parseInt(prompt("¿De qué número te gustaría aprender?"));
+  for (let i = 1; i <= 10; i++) {
+    let resultado = ingresarNumero * i;
+    alert(ingresarNumero + " X " + i + " = " + resultado);
+  }
+
+  respuesta = prompt("¿Deseas volver a comenzar? (responde 'si' o 'no')");
+} while (respuesta.toLowerCase() === "si");
