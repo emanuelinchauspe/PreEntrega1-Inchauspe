f3c89df37a87fa8450fa17d0128bdc77abf15999
Emanuel Inchauspe <ema.inchauspe@outlook.es>
2023-06-16 21:00:40 -0300

    Correciones realizadas

--- javascript.js
+++ javascript.js
@@ -1,12 +1,22 @@
-let respuesta;
-do {
-  alert("Vamos a aprender las tablas!");
+function aprenderTablas() {
+    let respuesta;
+    do {
+        alert("Vamos a aprender las tablas!");
 
-  let ingresarNumero = parseInt(prompt("¿De qué número te gustaría aprender?"));
-  for (let i = 1; i <= 10; i++) {
-    let resultado = ingresarNumero * i;
-    alert(ingresarNumero + " X " + i + " = " + resultado);
-  }
+        let ingresarNumero = parseInt(prompt("¿De qué número te gustaría aprender?"));
 
-  respuesta = prompt("¿Deseas volver a comenzar? (responde 'si' o 'no')");
-} while (respuesta.toLowerCase() === "si");
+        if (isNaN(ingresarNumero)) {
+            alert("Valor inválido. Por favor, ingresa un número.");
+            continue; 
+        }
+
+        for (let i = 1; i <= 10; i++) {
+            let resultado = ingresarNumero * i;
+            alert(ingresarNumero + " X " + i + " = " + resultado);
+        }
+
+        respuesta = prompt("¿Deseas volver a comenzar? (responde 'si' o 'no')");
+    } while (respuesta.toLowerCase() === "si");
+}
+
+aprenderTablas();
